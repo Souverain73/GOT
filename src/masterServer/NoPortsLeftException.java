68700f59496fea5593f8ba7fa7271ee3ab7751e8
Souverain73 <souverain73@gmail.com>
2017-02-21 08:11:06 +0300

    Master server moved to standalone project.

--- src/masterServer/NoPortsLeftException.java
+++
@@ -1,7 +0,0 @@
-package masterServer;
-
-/**
- * Created by Souverain73 on 14.02.2017.
- */
-public class NoPortsLeftException extends Throwable {
-}
--- src/masterServer/PlayerInfo.java
+++
@@ -1,8 +0,0 @@
-package masterServer;
-
-/**
- * Created by Souverain73 on 14.02.2017.
- */
-public class PlayerInfo {
-    public String nickname;
-}
